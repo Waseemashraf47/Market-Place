d67f78f591c3f289265094fbee9d46e89fb3dc6d
waseem <waseemashraf47@gmail.com>
2024-08-08 16:44:58 +0500

    Sign up form has been implemented

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
+from .forms import SignupForm
 # Create your views here.
+
+
 def index (request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
@@ -11,3 +14,16 @@ def index (request):
 
 def contact (request):
     return render (request, 'core/contact.html')
+
+def signup(requset):
+    if requset.method == 'POST':
+        form = SignupForm(requset.POST)
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/login/')
+    else:    
+        form = SignupForm()
+        return render (requset, 'core/signup.html', {
+            'form': form
+        })
